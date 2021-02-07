ec1c6cb424ddb716dab13837c4ace1d7621a22b1
Laurynas Valenta <valenta.laurynas@gmail.com>
2021-02-07 21:53:04 +0200

    Ensure that DB container is ready before attempting the database creation.

--- app/tools/init.sh
+++ app/tools/init.sh
@@ -1,5 +1,7 @@
 composer install
 
+./tools/wait-for-it.sh db:3306 -- echo "DB is up and running."
+
 bin/console doctrine:database:drop --force
 bin/console doctrine:database:create
 bin/console doctrine:schema:create
